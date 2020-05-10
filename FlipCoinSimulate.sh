c324ad9d9d6150f9f77e776c2b3c081b054c063d
mohammed92-max <shahrukhkhandb@gmail.com>
2020-05-10 12:12:45 +0530

    pushed UseCase4

--- FlipCoinSimulate.sh
+++ FlipCoinSimulate.sh
@@ -7,11 +7,14 @@ tails=0
 for ((count=0; count<noOfTimes; count++))
 do
         tossStatus=$((RANDOM%2))
+
         if [ $tossStatus -eq 1 ]
         then
                 ((heads++))
+		echo "heads= "$heads;
         else
                 ((tails++))
+		echo "tails= "$tails;
 	fi
 
 	if [[ $heads -eq 21 ]]
@@ -25,6 +28,21 @@ do
                 echo "Tail is winner at 21 times"
 		break;
         fi
+
+	if [[ $heads -eq $tails ]]
+	then
+		if [[ $(($heads-$tails)) -ge 2 ]]
+		then
+			echo "After tie,head has won with difference of minimum 2 points"
+			break;
+		fi
+
+		if [[ $(($tails-$heads)) -ge 2 ]]
+                then
+                        echo "After tie,tail has won with difference of minimum 2 points"
+                        break;
+                fi
+	fi
 
 done
 
